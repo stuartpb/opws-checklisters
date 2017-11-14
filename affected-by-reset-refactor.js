36a7cb1a0f14247798b53af2dc54fb9d907a73cf
Stuart P. Bentley <stuart@testtrack4.com>
2017-11-14 00:06:30 +0000

    Add uncommitted scripts from old domainprofiles workspace

---
+++ affected-by-reset-refactor.js
@@ -0,0 +1,17 @@
+var yaml = require('js-yaml');
+var fs = require('fs');
+var glob = require('glob');
+
+glob.sync('profiles/*', {cwd: __dirname + '/..'})
+  .forEach(function(filename) {
+    var domain = filename.replace(/^profiles\/([^\/]+)\.yaml$/,'$1');
+    var profile = yaml.load(
+      fs.readFileSync(filename, 'utf8'), {filename: filename});
+    if (profile.password && profile.password.reset &&
+      (profile.password.reset.token && profile.password.reset.token.login))
+    console.log ('- ' +
+      (profile.reviewed && profile.reviewed.date ? '[ ]' : '[ ]') +
+      ' [' + domain +
+      '](https://github.com/opensets/domainprofiles/blob/master/' +
+      filename + ')');
+});
